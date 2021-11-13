6afc6ee8d4a21c0550037e8fdf47679342d6cf12
roland.ormrod <roland.ormrod@fluidly.com>
2021-11-13 14:31:47 +0000

    Adds failing tests for bad post requests with incomplete data

--- src/resources/cars/cars.spec.ts
+++ src/resources/cars/cars.spec.ts
@@ -14,6 +14,55 @@ describe('POST /cars', () => {
       .post('/cars').send(body)
       .expect(200)
   })
+
+  it('responds with a 400 when posted with a blank car make', () => {
+    const body = {
+      make: '',
+      model: 'Astra SRi',
+      colour: 'Silver',
+      year: 2007,
+    }
+    return request(app)
+      .post('/cars').send(body)
+      .expect(400)
+  })
+
+  it('responds with a 400 when posted with a blank car model', () => {
+    const body = {
+      make: 'Vauxhall',
+      model: '',
+      colour: 'Silver',
+      year: 2007,
+    }
+    return request(app)
+      .post('/cars').send(body)
+      .expect(400)
+  })
+
+  it('responds with a 400 when posted with a blank car colour', () => {
+    const body = {
+      make: 'Vauxhall',
+      model: 'Astra SRi',
+      colour: '',
+      year: 2007,
+    }
+    return request(app)
+      .post('/cars').send(body)
+      .expect(400)
+  })
+
+  it('responds with a 400 when posted with a null car production year', () => {
+    const body = {
+      make: 'Vauxhall',
+      model: 'Astra SRi',
+      colour: 'Red',
+      year: null,
+    }
+    return request(app)
+      .post('/cars').send(body)
+      .expect(400)
+  })
+
 })
 
 describe('GET /cars/:id', () => {
@@ -45,6 +94,5 @@ describe('GET /cars/:id', () => {
     expect(getResponse.body.model).toBe('Fiesta 1.1l')
     expect(getResponse.body.colour).toBe('Blue')
     expect(getResponse.body.year).toBe(2008)
-
   })
 })
