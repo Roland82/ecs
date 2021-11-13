0c60a852e510b1fb853a0e03bcfc1116c6abdccc
roland.ormrod <roland.ormrod@fluidly.com>
2021-11-13 14:11:49 +0000

    Adds failing test for GET /cars/:id

--- src/resources/cars/cars.spec.ts
+++ src/resources/cars/cars.spec.ts
@@ -3,23 +3,48 @@ import app from '../../app'
 
 
 describe('POST /cars', () => {
-  it('responds with a 200 when posted to with all the required data to create a new car', async () => {
+  it('responds with a 200 when posted to with all the required data to create a new car', () => {
     const body = {
       make: 'Vauxhall',
       model: 'Astra SRi',
       colour: 'Silver',
       year: 2007,
     }
-    await request(app)
+    request(app)
       .post('/cars').send(body)
       .expect(200)
   })
 })
 
 describe('GET /cars/:id', () => {
-  it('responds with a 404 when a car with the id cannot be found', async () => {
-    await request(app)
+  it('responds with a 404 when a car with the id cannot be found', () => {
+    request(app)
       .get('/cars/10000000000').send()
       .expect(404)
+  })
+
+  it('responds with a 200 and the car data when a previously saved car is found', async () => {
+    const carBody = {
+      make: 'Ford',
+      model: 'Fiesta 1.1l',
+      colour: 'Blue',
+      year: 2008,
+    }
+    const postResponse = await request(app)
+      .post('/cars').send(carBody)
+      .expect(200)
+
+    const carResourceLocation = postResponse.headers['Content-Location']
+
+    const getResponse = await request(app)
+      .get(carResourceLocation).send()
+      .expect(200)
+
+    expect(getResponse.body.id).not.toBeNull()
+    expect(getResponse.body.make).toBe('Ford')
+    expect(getResponse.body.model).toBe('Fiesta 1.1l')
+    expect(getResponse.body.colour).toBe('Blue')
+    expect(getResponse.body.year).toBe(2008)
+
   })
 })
