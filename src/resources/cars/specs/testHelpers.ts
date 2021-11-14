5c70bc96f400a3abcaf337ba11947cf0d3edfbba
roland.ormrod <roland.ormrod@fluidly.com>
2021-11-14 14:34:51 +0000

    Fixing another issue after installing from scratch and running

--- src/resources/cars/specs/testHelpers.ts
+++ src/resources/cars/specs/testHelpers.ts
@@ -1,4 +1,5 @@
 import nock from 'nock'
+import {DataMuseSimilarWordsResponseBody} from '../types'
 
 export const mockDataMuseApi = (options: { carMake: string, returnStatusCode: number, responseBody: DataMuseSimilarWordsResponseBody }) => {
   const dataMuseApiMock = nock('https://api.datamuse.com')
